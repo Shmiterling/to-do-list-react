c2402a1249365666ae68cf1c487e2e7b865ab7bc
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2022-07-08 14:43:13 +0200

    0.1.15
    login

--- src/components/login/LogIn.tsx
+++ src/components/login/LogIn.tsx
@@ -2,6 +2,8 @@ import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 import { createRef, useState } from "react";
+import axios from "axios";
+import qs from 'qs';
 
 export default function LogIn(): JSX.Element {
 
@@ -18,9 +20,61 @@ export default function LogIn(): JSX.Element {
     }
 
     const logIn = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        navigate('/to-do-list-react/app')
+
+        let email = (_email.current !== null ? _email.current.value : '');
+        let password = (_password.current !== null ? _password.current.value : '');
+
+        // let data = {
+        //     email: email,
+        //     password: password
+        // };
+        // let data = "email=admin%40email.com&password=qwerty123";
+        // let axiosConfig = {
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/x-www-form-urlencoded'
+        //     }
+        // };
+
+
+        // let qs = require('qs');
+
+
+        // axios.post('https://todo.coldwinternight.ru/api/login',
+        //     data,
+        //     axiosConfig)
+        //     .then((res) => {
+        //         console.log(res)
+        //     })
+        //     .catch((err) => {
+        //         console.log(err)
+        //     })
+        // navigate('/to-do-list-react/app')
+
+        const qs = require('qs');
+        let data = qs.stringify({
+            'email': 'admin@email.com',
+            'password': 'qwerty123'
+        });
+        let config = {
+            method: 'post',
+            url: 'https://todo.coldwinternight.ru/api/login',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: data
+        };
+        console.log(data)
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     return (
